2b6a0c050ba664c52cd206fd49aa92ead1e4edc6
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-11-23 10:37:47 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let myButton = document.querySelectorAll("form input")[1];
+
+  var form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    myInputElement = document.querySelector("#new-task-description");
+
+    var shitVarmi = controlMessage(myInputElement.value);
+    if (!shitVarmi) {
+      listAdder(myInputElement.value);
+      clearInput(myInputElement);
+    } else alert("shit giremezsin");
+  });
+
+  function controlMessage(messageParam) {
+    // monitor specific keywords
+    if (messageParam == "shit") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function listAdder(givenListName) {
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(givenListName));
+    ul.appendChild(li);
+  }
+
+  function clearInput(inputElement) {
+    inputElement.value = "";
+  }
 });
