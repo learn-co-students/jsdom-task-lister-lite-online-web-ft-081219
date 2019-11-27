04d2ebc6456e2725e17440d63922295254bb6626
KHong <albtrossfacefall@gmail.com>
2019-11-26 19:39:57 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let form = document.getElementById('create-task-form');
-  let input = document.getElementById('new-task-description');
-  let list = document.getElementById('tasks');
+  giveFormColorPicker();
+  giveFormTaskEvent();
+});
 
+const form = document.getElementById('create-task-form');
+const list = document.getElementById('tasks');
+const input = document.getElementById('new-task-description');
+
+function newLiElement() {
+  const newTask = document.createElement('li');
+  newTask.textContent = input.value + ' ';
+  newTask.style.color = form.querySelector('select').value;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+
+  newTask.appendChild(deleteButton);
+  deleteButton.addEventListener('click', function(event) {
+    list.removeChild(newTask);
+  });
+  return newTask;
+};
+
+function giveFormTaskEvent() {
   form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    let newTask = document.createElement('li');
-    newTask.textContent = input.value + ' ';
+    const task = newLiElement();
 
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'X';
-    newTask.appendChild(deleteButton);
-    deleteButton.addEventListener('click', function(event) {
-      list.removeChild(newTask);
-    });
-
-    list.appendChild(newTask);
+    list.appendChild(task);
     input.value = '';
   });
-});
+};
+
+function makeOpt(arg) {
+  const opt = document.createElement('option');
+  opt.value = arg;
+  opt.textContent = arg;
+  return opt;
+}
+
+function giveFormColorPicker() {
+  const inp = document.createElement('select');
+  inp.name = 'color';
+  inp.appendChild(makeOpt('red'));
+  inp.appendChild(makeOpt('yellow'));
+  inp.appendChild(makeOpt('green'));
+  form.insertBefore(inp, form.querySelector('[type="submit"]'));
+}
