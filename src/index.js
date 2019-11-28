f04c95f023d1b222f5cd6d8e205fcaf9b318a41e
Caleb Kavanaugh <calebck@hotmail.com>
2019-11-27 23:16:00 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const taskForm = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#tasks")
+
+taskForm.addEventListener("submit", (event) =>{
+  event.preventDefault()
+  const newTask = document.querySelector("#new-task-description").value
+  // const taskItem = document.createElement("li")
+  
+  taskList.innerHTML += `
+    <li>${newTask}  
+      <button data-action="delete">Delete</button>
+    </li>
+  `
+  taskForm.reset()
+} )
+
+taskList.addEventListener("click", function(event) {
+  console.log(event.target)
+  if (event.target.dataset.action === "delete") {
+    event.target.parentElement.remove()
+  } 
+})
+
